5dae06b0cb552e0e91ef2785a14e2a5ffa5cb348
Victor Hugo <vhaporfiro@gmail.com>
2025-05-18 07:06:31 -0300

    Configuração do gradle

---
+++ Projeto integrador de modulo/src/main/java/br/com/projetoIntegrador/ProjetoIntegradorDeModuloApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorDeModuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorDeModuloApplication.class, args);
+	}
+
+}
---
+++ Projeto integrador de modulo/src/test/java/br/com/projetoIntegrador/ProjetoIntegradorDeModuloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorDeModuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
